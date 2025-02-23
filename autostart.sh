6c4291cb084475210e7ed3fc6e6f0b9e7bdf8766
pedro <p3drov1n1cyus@gmail.com>
2025-02-23 19:26:44 -0300

    start config

--- autostart.sh
+++ autostart.sh
@@ -2,3 +2,10 @@
 
 nitrogen --restore &
 picom &
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+PS1='[\u@\h \W]\$ '
+
+export PATH=$PATH:$HOME/bin
+xrandr --output HDMI-A-0 --primary --output DisplayPort-0 --right-of HDMI-A-0
